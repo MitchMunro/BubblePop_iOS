e331aa8bbe4f469427f1ffe8aa81054c047f1e10
MitchMunro <mitchmunro@gmail.com>
2024-04-02 13:45:29 +1100

    Initial Commit

---
+++ BubblePopAssessment/BubblePopAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopAssessmentApp.swift
+//  BubblePopAssessment
+//
+//  Created by Mitch Munro on 2/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubblePopAssessment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BubblePopAssessment
+//
+//  Created by Mitch Munro on 2/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
